25ea1bfd63ea85f02c4aa1b7a472593f1e6da90f
devbeau <48367657+devbeau@users.noreply.github.com>
2020-07-24 12:05:15 -0400

    All MVP Done

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -22,23 +22,26 @@ The zoos want to display both the scientific name and the animal name in front o
 */
 const displayNames = [];
 console.log(displayNames);
-
+zooAnimals.forEach((item) => displayNames.push(`${item.animal_name}, ${item.scientific_name}.`))
+console.log("This is Request 1 from arrays-callbacks.js:", displayNames)
 /* Request 2: .map()
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
 
-const lowCaseAnimalNames
-console.log(lowCaseAnimalNames);
+
+let lowCaseAnimalNames = zooAnimals.map((item) => `${item.animal_name.toLowerCase()}`);
+
+console.log("This is Request 2 from arrays-callbacks.js:", lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
-console.log(lowPopulationAnimals);
+const lowPopulationAnimals = zooAnimals.filter((item) => item.population < 5);
+console.log("This is Request 3 from arrays-callbacks.js:", lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
@@ -46,7 +49,8 @@ The zoos need to know their total animal population across the United States. Fi
 
 */
 let populationTotal = 0;
-console.log(populationTotal);
+populationTotal = zooAnimals.reduce((acc, cur) => acc + cur.population, 0);
+console.log("This is Request 4 from arrays-callbacks.js:", populationTotal);
 
 
 // ==== Callbacks ====  
@@ -57,19 +61,23 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb){
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+function add(a, b) {return a + b};
+function multiply(a, b){return a * b};
+function greeting(first, last){return `Hello ${first} ${last}, nice to meet you!`};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log("These are the tests for arrays-callbacks.js", consume(2, 2, add)); // 4
+console.log("These are the tests for arrays-callbacks.js", consume(10, 16, multiply)); // 160
+console.log("These are the tests for arrays-callbacks.js", consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
